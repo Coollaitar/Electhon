b0c5dbacad46628d945f714adc2d9c16616fccf7
Aadit Palande <aadit2002nov@gmail.com>
2023-04-16 23:03:28 +0530

    Working Code Added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-import mysql from 'mysql2';
-import express, { query } from 'express';
-import nodemon from 'nodemon';
-
+const mysql = require('mysql2');
+const express = require('express');
+const query = require('express');
+const nodemon = require('nodemon');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -12,14 +13,25 @@ const db = mysql.createConnection({
   password: '@adit27nov',
   database: 'aadhar',
 });
+const accountSid = 'ACe2a9003302f9bdff9a9bc259dbf03399';
+const authToken = '7a240c3c060cf27c09f7071682f4cd32';
+const client = require('twilio')(accountSid, authToken);
+client.messages
+  .create({
+    body: 'Authentication OTP is 1211 . Please enter OTP and then click on this link http://localhost:3000/details/123',
+    from: '+15075686894',
+    to: '+918355983607',
+  })
+  .then((message) => console.log(message.sid));
 
 app.get('/', (req, res) => {
   res.json('Backend check');
 });
-app.get('/details', (req, res) => {
-  const q = 'SELECT * FROM details';
+app.get('/details/:aadharNumber', (req, res) => {
+  const aadharNumber = req.params.aadharNumber;
+  const q = 'SELECT * FROM details WHERE aadharNumber=?';
 
-  db.query(q, (err, data) => {
+  db.query(q, aadharNumber, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
@@ -39,7 +51,7 @@ app.put('/details/:aadharNumber/add', (req, res) => {
 app.put('/details/:aadharNumber/relativeUpdate', (req, res) => {
   const aadharNumber = req.params.aadharNumber;
   const q =
-    'UPDATE details SET `relativeFirstName`= ? ,`relativeSurname`= ?,`realtiveNumber`= ? WHERE aadharNumber= ?';
+    'UPDATE details SET `relativeFirstName`= ? ,`relativeSurname`= ?,`realtiveNumber`= ? ,`typeofRelation` = ? WHERE aadharNumber= ?';
   const value = [
     req.body.relativeFirstName,
     req.body.relativeSurname,
@@ -51,6 +63,44 @@ app.put('/details/:aadharNumber/relativeUpdate', (req, res) => {
     return res.json(data);
   });
 });
+// Query 1 @condition-> below 18 for address
+app.get('/query1/:UID', (req, res) => {
+  const aadharNumber = req.params.aadharNumber;
+  const age = req.params.AGE;
+  const q =
+    'SELECT epic.Address FROM epic  JOIN details ON epic.epicNumber = details.realtiveEPICNum WHERE details.AGE < 18';
+
+  db.query(q, aadharNumber, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+// Query 2 @condition-> fetch address form passport AGE>18
+app.get('/query2/:UID', (req, res) => {
+  const aadharNumber = req.params.aadharNumber;
+  const age = req.params.AGE;
+  const q =
+    'SELECT passport.Address FROM passport  JOIN details ON passport.UID = details.UID WHERE details.UID=12';
+
+  db.query(q, aadharNumber, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+// Query @condition-> aadhar is not linked with mobile so get data from bc & address from govt verified docs
+app.get('/query3/:UID', (req, res) => {
+  const aadharNumber = req.params.aadharNumber;
+  const age = req.params.AGE;
+  const q =
+    'SELECT rationcard.Address FROM rationcard  JOIN details ON rationcard.UID = details.UID WHERE details.UID=214';
+
+  db.query(q, aadharNumber, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.listen(8800, () => {
   console.log('Connected to backend');
 });
